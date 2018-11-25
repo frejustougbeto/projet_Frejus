50562882b94933aa63c167fe4299afb2d56b6fbb
Frejus TOUGBETO <frejustougbeto@yahoo.fr>
2018-11-25 13:26:31 +0100

    Ajout du fichier fich_semaine_derniere.sh

---
+++ fich_semaine_derniere.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello $USERNAME, nous sommes le $(date +"%d %B %Y")."
+read -p  "Veuillez entrer le chemin du repertoire souhaité: " repertoire ###recueillir le chemin du repertoire
+echo "  "   ### Saut de lignes pour plus de clarté
+ls $repertoire  ### Affichage du contenu du repertoire
+ 
